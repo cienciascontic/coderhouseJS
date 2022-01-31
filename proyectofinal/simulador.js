6b844daa57a45a72943d84687b1152ada52a61a5
Cristián Rizzi <rizzi.cristian@gmail.com>
2022-01-31 00:54:38 -0300

    Update simulador.js

--- proyectofinal/simulador.js
+++ proyectofinal/simulador.js
@@ -1,138 +1,145 @@
 /*jshint esversion: 6 */
+
+// window.onload = function() {
+//   comenzarJuego();
+// };
+
+//con jQuery
+$(document).ready(function() {
+  comenzarJuego();
+})
+
 let puntajeUsuario = 0;
 let respuestaCorrecta;
 let catNomCompleto;
 let iconoCategoria;
 let claseAnterior;
-let tiempoDisponible = 500;
+const tiempoParaVerLaRespuesta = 1500;
 let textoSetearTiempo;
 // para guardar los usuarios y sus puntajes en localStorage
 let todosLosUsuarios = [];
 let todosLosPuntajes = [];
-
-function ocultarPreguntaYBoton() {
-document.getElementById('contenedorPregunta').style.visibility = "hidden";
-document.getElementById('idBotonHacerPregunta').style.visibility = "hidden";
-  seteoInicial();
-}
-
-function seteoInicial() {
-
-  let divUsuario = document.getElementById("idDivUsuario");
-  //  texto para ingresar nombre de usuario
-  let textoUsuario = document.createElement("div");
-  textoUsuario.innerHTML = "Ingrese su nombre de usuario:";
-  textoUsuario.style.textAlign = "center";
-
-  divUsuario.appendChild(textoUsuario);
-
-  let divParental = document.getElementById("idDivUsuario");
-  // input de nombre de usuario
-  let inputNombreDeUsuario = document.createElement("input");
-  inputNombreDeUsuario.setAttribute("type", "text");
-  inputNombreDeUsuario.setAttribute("id", "idNombreDeUsuario");
-
-  divParental.appendChild(inputNombreDeUsuario);
-
-  document.getElementById("idNombreDeUsuario").addEventListener('focusout', eventoUsuario => {
-      nombreDeUsuario = eventoUsuario.currentTarget.value;
-      let losUsuariosGuardados = [];
-      if (localStorage.getItem("usuariosGuardados") === null) {
-        losUsuariosGuardados.push(nombreDeUsuario);
-        localStorage.setItem("usuariosGuardados", JSON.stringify(losUsuariosGuardados));
-      } else {
-        losUsuariosGuardados = JSON.parse(localStorage.getItem("usuariosGuardados"));
-        losUsuariosGuardados.push(nombreDeUsuario);
-        localStorage.setItem("usuariosGuardados", JSON.stringify(losUsuariosGuardados));
-      }
-
-  })
-
-  let divPadre = document.getElementById("idDivClaseTiempo");
-  // input de tiempo de juego
-  let tiempoDeJuego = document.createElement("input");
-  tiempoDeJuego.setAttribute("type", "number");
-  tiempoDeJuego.setAttribute("id", "idInputTiempoDeJuego");
-
-  divPadre.appendChild(tiempoDeJuego);
-
-  // texto de tiempo de juego
-  textoSetearTiempo = document.createElement("div");
-  textoSetearTiempo.innerHTML = "Ingresar tiempo de juego en segundos y luego dar ENTER";
-  textoSetearTiempo.style.textAlign = "center";
-
-  let divActual = document.getElementById("idDivClaseTiempo");
-  divActual.style.textAlign = "center";
-  document.body.insertBefore(textoSetearTiempo, divActual);
-
-    document.getElementById("idInputTiempoDeJuego").addEventListener('keyup', evento => {
-      if (evento.keyCode === 13) {
-        tiempoDisponible = parseInt(evento.currentTarget.value);
-        comenzarJuego();
-      }
-    })
-}
+let bolsaDePreguntas = [];
+let tiempoDisponible;
+let nombreDeUsuario;
 
 function comenzarJuego() {
   document.getElementById('contenedorPregunta').style.visibility = "visible";
-  document.getElementById('idBotonHacerPregunta').style.visibility = "visible";
-  setTimeout(controlDeTiempo, tiempoDisponible*1000);
+  recuperarUsuarios();
+  establecerTiempo();
+  definirPreguntas();
   hacerPregunta();
+};
+
+function recuperarUsuarios() {
+  todosLosUsuarios = JSON.parse(localStorage.getItem("usuariosGuardados"));
+  let cantidadDeUsuariosGuardados = todosLosUsuarios.length;
+  let ultimoUsuario = todosLosUsuarios[cantidadDeUsuariosGuardados - 1];
+  nombreDeUsuario = ultimoUsuario;
 }
 
-class Pregunta {
-  constructor(idPregunta, categoria, ayuda, enunciado, opciones = [], opcionCorrecta) {
-    this.idPregunta = idPregunta;
-    this.categoria = categoria;
-    this.ayuda = ayuda;
-    this.enunciado = enunciado;
-    this.opciones = opciones;
-    this.opcionCorrecta = opcionCorrecta;
-    this.respondida = false;
-    this.sinOpciones = false;
+function establecerTiempo() {
+  todosLosTiempos = JSON.parse(localStorage.getItem("tiemposGuardados"));
+  let cantidadDeTiemposGuardados = todosLosTiempos.length;
+  let tiempoDisponible = todosLosTiempos[cantidadDeTiemposGuardados - 1];
+  setTimeout(controlDeTiempo, tiempoDisponible*1000);
+};
+
+function definirPreguntas() {
+  class Pregunta {
+    constructor(idPregunta, categoria, ayuda, enunciado, opciones = [], opcionCorrecta) {
+      this.idPregunta = idPregunta;
+      this.categoria = categoria;
+      this.ayuda = ayuda;
+      this.enunciado = enunciado;
+      this.opciones = opciones;
+      this.opcionCorrecta = opcionCorrecta;
+      this.respondida = false;
+      this.sinOpciones = false;
+    }
+    responder() {
+      this.respondida = true;
+    }
+    sinOpciones() {
+      this.sinOpciones = true;
+    }
+
+  }// fin del constructor}
+
+  bolsaDePreguntas.push(new Pregunta(1,"HIS","https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Alexander_the_Great_mosaic.jpg/250px-Alexander_the_Great_mosaic.jpg","¿A qué le temía Bucéfalo?", ["A los rebencazos de su amo", "A los ratones", "A su sombra", "Al agua"], "opcion3"));
+
+  bolsaDePreguntas.push(new Pregunta(2,"TEC","https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg/800px-LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg","¿Quién se apellida Torvalds?", ["El creador de Wikipedia", "El creador de Linux", "El creador de Twicth", "El creador del pendrive"], "opcion2"));
+
+  bolsaDePreguntas.push(new Pregunta(3,"DEP","https://cdn.pixabay.com/photo/2016/07/28/15/28/winner-1548239_1280.jpg","¿Cuál de estos deportes le otorgó más medallas olímpicas a la Argentina en toda su historia?", ["Hockey sobre césped", "Atletismo", "Tenis", "Boxeo"], "opcion4"));
+
+  bolsaDePreguntas.push(new Pregunta(4,"TEC", "imgs/img-pregunta4.jpg", "¿Qué modelo es esta computadora?", ["Commodore 64", "Atari 130XE", "Talent MSX", "IBM PS 2"], "opcion2"));
+};
+
+function hacerPregunta() {
+  let cantPreguntas = bolsaDePreguntas.length;
+  let indicePregunta = numeroAleatorio(0 , cantPreguntas - 1);
+
+  switch (bolsaDePreguntas[indicePregunta].categoria) {
+    case "HIS":
+    catNomCompleto = "Historia";
+    iconoCategoria = '<i class="fas fa-landmark fa-2x"></i>';
+    break;
+    case "TEC":
+    catNomCompleto = "Tecnología";
+    iconoCategoria = '<i class="fas fa-laptop fa-2x"></i>';
+    break;
+    case "DEP":
+    catNomCompleto = "Deportes";
+    iconoCategoria = '<i class="fas fa-running fa-2x"></i>';
+    break;
+    default:
   }
-  responder() {
-    this.respondida = true;
+
+  document.getElementById("imagenPregunta").src = bolsaDePreguntas[indicePregunta].ayuda;
+  document.getElementById("idEnunciado").innerHTML = bolsaDePreguntas[indicePregunta].enunciado;
+  // ícono de la categoría temática
+  let divDeLaCategoriaTematica = document.getElementById("idDivCategoriaTematica");
+  divDeLaCategoriaTematica.innerHTML = iconoCategoria + ' &nbsp &nbsp' + catNomCompleto;
+  // crear las opciones dinámicamente
+  let divDeLasOpciones = document.getElementById("opcionesDeLasPreguntas");
+  divDeLasOpciones.innerHTML = "";
+
+  for (var i = 0; i < bolsaDePreguntas.length; i++) {
+    let j = i + 1;
+    let opcion = "opcion" + j;
+
+    //agregado para crear las opciones dinámicamente
+    let opcionDinamica = document.createElement("div");
+    opcionDinamica.setAttribute("id", opcion);
+    opcionDinamica.setAttribute("class", "opcionPregunta");
+    opcionDinamica.setAttribute("onclick", "evaluarRespuesta()");
+    divDeLasOpciones.appendChild(opcionDinamica);
+
+    document.getElementById(opcion).innerHTML = bolsaDePreguntas[indicePregunta].opciones[i];
   }
-  sinOpciones() {
-    this.sinOpciones = true;
-  }
 
-}// fin del constructor}
+  respuestaCorrecta = bolsaDePreguntas[indicePregunta].opcionCorrecta;
 
-const bolsaDePreguntas = [];
-bolsaDePreguntas.push(new Pregunta(1,"HIS","https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Alexander_the_Great_mosaic.jpg/250px-Alexander_the_Great_mosaic.jpg","¿A qué le temía Bucéfalo?", ["A los rebencazos de su amo", "A los ratones", "A su sombra", "Al agua"], "opcion3"));
-
-bolsaDePreguntas.push(new Pregunta(2,"TEC","https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg/800px-LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg","¿Quién se apellida Torvalds?", ["El creador de Wikipedia", "El creador de Linux", "El creador de Twicth", "El creador del pendrive"], "opcion2"));
-
-bolsaDePreguntas.push(new Pregunta(3,"DEP","https://cdn.pixabay.com/photo/2016/07/28/15/28/winner-1548239_1280.jpg","¿Cuál de estos deportes le otorgó más medallas olímpicas a la Argentina en toda su historia?", ["Hockey sobre césped", "Atletismo", "Tenis", "Boxeo"], "opcion4"));
-
-bolsaDePreguntas.push(new Pregunta(4,"TEC", "imgs/img-pregunta4.jpg", "¿Qué modelo es esta computadora?", ["Commodore 64", "Atari 130XE", "Talent MSX", "IBM PS 2"], "opcion2"));
+}; //fin de función hacerPregunta
 
 function numeroAleatorio(min, max) {
    return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 function controlDeTiempo() {
   document.getElementById('contenedorPregunta').style.visibility = "hidden";
-  document.getElementById('idBotonHacerPregunta').style.visibility = "hidden";
-  document.getElementById('idDivClaseTiempo').style.visibility = "hidden";
-  textoSetearTiempo.innerHTML = "Se agotó el tiempo. Obtuviste: " + puntajeUsuario + " puntos";
+  textoResultado = document.getElementById("idDivResultado");
+  textoResultado.innerHTML = "Se agotó el tiempo " + nombreDeUsuario + ". Obtuviste: " + puntajeUsuario + " puntos";
 
-  let losPuntajesGuardados = [];
-
-  if (localStorage.getItem("puntajesGuardados") === null) {
-    losPuntajesGuardados.push(puntajeUsuario);
-    localStorage.setItem("puntajesGuardados", JSON.stringify(losPuntajesGuardados));
-  } else {
-    losPuntajesGuardados = JSON.parse(localStorage.getItem("puntajesGuardados"));
-    losPuntajesGuardados.push(puntajeUsuario);
-    localStorage.setItem("puntajesGuardados", JSON.stringify(losPuntajesGuardados));
-  }
+  guardarPuntajesEnLocalStorage();
 
 let divPadreCuadroDeHonor = document.getElementById("idDivCuadroDeHonor");
 let usuariosDelCuadro = JSON.parse(localStorage.getItem("usuariosGuardados"));
+let usuariosDelCuadroInvertidos = usuariosDelCuadro.reverse();
 let puntajesDelCuadro = JSON.parse(localStorage.getItem("puntajesGuardados"));
+let puntajesDelCuadroInvertidos = puntajesDelCuadro.reverse();
+let tiemposDelCuadro = JSON.parse(localStorage.getItem("tiemposGuardados"));
+let tiemposDelCuadroInvertidos = tiemposDelCuadro.reverse();
 
 let tablaCuadroDeHonor = document.createElement('table');
 let encabezadoTablaCuadroDeHonor = document.createElement('thead');
@@ -162,60 +169,46 @@ encabezadoTablaCuadroDeHonor.appendChild(encabezado);
 for (var i = 0; i < usuariosDelCuadro.length; i++) {
 
     let fila = document.createElement('tr');
+    //fila.setAttribute('id', 'fila' + i);
     let filaUsuario = document.createElement('td');
-    filaUsuario.innerHTML = usuariosDelCuadro[i];
+    filaUsuario.setAttribute('id', 'filaUsuario' + i);
+    filaUsuario.innerHTML = usuariosDelCuadroInvertidos[i];
     let filaPuntaje = document.createElement('td');
-    filaPuntaje.innerHTML = puntajesDelCuadro[i];
+    filaPuntaje.setAttribute('id', 'filaPuntaje' + i);
+    filaPuntaje.innerHTML = puntajesDelCuadroInvertidos[i];
     let filaTiempo = document.createElement('td');
-    filaTiempo.innerHTML = "próximamente...";
+    filaTiempo.innerHTML = tiemposDelCuadroInvertidos[i];
+    filaTiempo.setAttribute('id', 'filaTiempo' + i);
 
     fila.appendChild(filaUsuario);
     fila.appendChild(filaPuntaje);
     fila.appendChild(filaTiempo);
     cuerpoTablaCuadroDeHonor.appendChild(fila);
 }
+    // resaltar la primera fila
+    let primeraCeldaUsuario = document.getElementById('filaUsuario0');
+    primeraCeldaUsuario.style.backgroundColor = "rgb(192,137,143,240)";
+
+    let primeraCeldaPuntaje = document.getElementById('filaPuntaje0');
+    primeraCeldaPuntaje.style.backgroundColor = "rgb(192,137,143,240)";
+
+    let primeraCeldaTiempo = document.getElementById('filaTiempo0');
+    primeraCeldaTiempo.style.backgroundColor = "rgb(192,137,143,240)";
 
 }; // fin de función controlDeTiempo()
 
-function hacerPregunta() {
-  let indicePregunta = numeroAleatorio(0,bolsaDePreguntas.length - 1);
-  claseAnterior = document.getElementById("idSpanIconoCategoria").classList.value;
+function guardarPuntajesEnLocalStorage() {
+    let losPuntajesGuardados = [];
 
-if (claseAnterior != "") {
-  document.getElementById("idSpanIconoCategoria").classList.value = "nada";
-  document.getElementById("idSpanIconoCategoria").classList.remove("nada");
-}
-  switch (bolsaDePreguntas[indicePregunta].categoria) {
-    case "HIS":
-    catNomCompleto = "Historia";
-    iconoCategoria = "icofont-university";
-    break;
-    case "TEC":
-    catNomCompleto = "Tecnología";
-    iconoCategoria = "icofont-computer";
-    break;
-    case "DEP":
-    catNomCompleto = "Deportes";
-    iconoCategoria = "icofont-runner";
-    break;
-    default:
-  }
-
-  document.getElementById("imagenPregunta").src = bolsaDePreguntas[indicePregunta].ayuda;
-  document.getElementById("idEnunciado").innerHTML = bolsaDePreguntas[indicePregunta].enunciado;
-  document.getElementById("idCategoria").innerHTML = catNomCompleto;
-
-  document.getElementById("idSpanIconoCategoria").classList.add(iconoCategoria, "icofont-2x");
-
-  for (var i = 0; i < bolsaDePreguntas.length; i++) {
-    let j = i + 1;
-    let opcion = "opcion" + j;
-    document.getElementById(opcion).innerHTML = bolsaDePreguntas[indicePregunta].opciones[i];
-  }
-
-  respuestaCorrecta = bolsaDePreguntas[indicePregunta].opcionCorrecta;
-
-}
+    if (localStorage.getItem("puntajesGuardados") === null) {
+      losPuntajesGuardados.push(puntajeUsuario);
+      localStorage.setItem("puntajesGuardados", JSON.stringify(losPuntajesGuardados));
+    } else {
+      losPuntajesGuardados = JSON.parse(localStorage.getItem("puntajesGuardados"));
+      losPuntajesGuardados.push(puntajeUsuario);
+      localStorage.setItem("puntajesGuardados", JSON.stringify(losPuntajesGuardados));
+    }
+};
 
 function evaluarRespuesta(){
 let opcionCliqueada = event.srcElement.id;
@@ -229,7 +222,7 @@ else {
   document.getElementById(opcionCliqueada).innerHTML = textoOpcionCliqueada + "   " + '<i class="fas fa-dizzy" style="color: red; font-weight: bold; font-size: 30px"></i>';
   puntajeUsuario --;
 }
-
+setTimeout(hacerPregunta,tiempoParaVerLaRespuesta);
 }
 
-ocultarPreguntaYBoton();
+//limpiarFondo();
